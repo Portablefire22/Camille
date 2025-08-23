405f2ec3410d8f24a05100b01b4c97ab33c39e6f
Portablefire22 <lilith@kitten.rs>
2025-08-23 16:39:02 +0100

    Added basicJid.
    For some reason Rider didnt include this already

---
+++ Camille/Xmpp/BasicJid.cs
@@ -0,0 +1,37 @@
+﻿namespace Camille.Xmpp;
+
+public class BasicJid
+{
+
+    private string _jid;
+    private string _username;
+    private string _password;
+
+    public bool IsAuthenticated { get; set; }
+
+    public string Jid
+    {
+        get => _jid;
+        set => _jid = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Username
+    {
+        get => _username;
+        set => _username = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => _password = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+
+    public BasicJid(string jid, string username, string password)
+    {
+        this._jid = jid;
+        this._username = username;
+        this._password = password;
+    }
+}
